d5d5efc57f3bdbdd3b47435f67672e17148c99ca
nabila <nabilatajrin@gmail.com>
2020-03-07 12:30:54 +0600

    update

--- input-output.py
+++ input-output.py
@@ -1,4 +1,7 @@
 x = 1
 str_x = str(x)
 
-print("My fav numb is " + str_x + ". " "x = " + str_x)
+print("My fav numb is ", x, ". ", "x = ", x)
+
+#can only concatenate str (not "int") to str
+print("My fav numb is " + str_x + ". " + "x = " + str_x)
