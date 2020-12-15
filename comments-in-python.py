56731e3a9a79f5761cda91eb519727e73fca8ff8
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-12-15 11:36:27 +0600

    Update comments-in-python.py

--- comments-in-python.py
+++ comments-in-python.py
@@ -1,8 +1,6 @@
 #A hash sign (#) that is not inside a string literal is the beginning of a comment. All characters after the #,
 # up to the end of the physical line, are part of the comment and the Python interpreter ignores them.
-
 #!/usr/bin/python3
-
 # First comment
 print ("Hello, Python!") # second comment
 
@@ -14,12 +12,10 @@ print ("Hello, Python!") # second comment
 name = "Madisetti" # This is again comment
 
 #You can comment multiple lines as follows −
-
 # This is a comment.
 # This is a comment, too.
 # This is a comment, too.
 # I said that already.
-
 #Following triple-quoted string is also ignored by Python interpreter and can be used as a multiline comments:
 
 '''
