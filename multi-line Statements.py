53d50658cef10d420a861db5d1f293c74f31f596
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-12-15 11:37:27 +0600

    Update multi-line Statements.py

--- multi-line Statements.py
+++ multi-line Statements.py
@@ -1,13 +1,10 @@
 item_one=1
 item_two=2
 item_three=3
-
 total = item_one + \
    item_two + \
    item_three
-
 print(total)
-
 
 #The statements contained within the [], {}, or () brackets do not need to use the line continuation character '\'.
 # For example −
